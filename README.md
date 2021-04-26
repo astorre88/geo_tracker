cc5999dad39f20cac1a3804da952da8e645d3c21
Dmitry Vysotsky <bigbenperl@gmail.com>
2021-04-26 07:55:51 +0300

    Add README and seeds

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # GeoTracker
+
+## Setup
+
+Fetch dependencies:
+
+```sh
+mix deps.get
+```
+
+Create DB and run migrations:
+
+```sh
+mix ecto.create && mix ecto.migrate && mix run priv/repo/seeds.exs
+```
+
+## Tests
+
+```sh
+mix test
+```
+
+## Run
+
+```sh
+iex -S mix phx.server
+```
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias GeoTracker.{Auth, Repo}
+alias GeoTracker.Accounts.Token
+alias GeoTracker.Tasks.Task
+
+%Token{}
+|> Token.changeset(%{value: Auth.generate_token(), role: :driver})
+|> Repo.insert()
+
+%Token{}
+|> Token.changeset(%{value: Auth.generate_token(), role: :manager})
+|> Repo.insert()
+
+%Task{}
+|> Task.changeset(%{state: :new, lat1: 55.82115010616766, long1: 37.498799148311896, lat2: 55.688011, long2: 37.784089})
+|> Repo.insert()
+
+%Task{}
+|> Task.changeset(%{state: :new, lat1: 55.817413291533725, long1: 37.49170692800352, lat2: 55.688011, long2: 37.784089})
+|> Repo.insert()
